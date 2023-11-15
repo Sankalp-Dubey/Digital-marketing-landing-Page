24ff7e858cccdd0afca1daf500b1cf6721d3f05d
Sankalp Dubey <sankalpdubey152002@gmail.com>
2023-11-15 16:19:46 +0530

    useclient

---
+++ src/app/metadata.js
@@ -0,0 +1,22 @@
+export const metadata = {
+    title: "Web & Mobile App Development Company in India | SpireHub",
+    description:
+      "SpireHubs offers end-to-end web mobile application development services such as business needs & requirements analysis & custom design architecture. ",
+    alternates: {
+      canonical: "https://www.spirehubs.com",
+    },
+    metadataBase: new URL("https://www.spirehubs.com"),
+    robots: {
+      index: true,
+      follow: true,
+    },
+    openGraph: {
+      title: "Web & Mobile App Development Company in India | SpireHub",
+      description:
+        "SpireHubs offers end-to-end web mobile application development services such as business needs & requirements analysis & custom design architecture. ",
+      url: "https://www.spirehubs.com",
+      siteName: "SpireHub",
+      locale: "en_US",
+      type: "website",
+    },
+  };
--- src/app/page.js
+++ src/app/page.js
@@ -23,28 +23,7 @@ import FooterCTA from "@/sections/home/FooterCTA";
 import SoftwareDetail from "@/sections/home/SoftwareDetail";
 import OurValuableClientsDynamic from "@/components/common/OurValuableClientsDynamic";
 
-export const metadata = {
-  title: "Web & Mobile App Development Company in India | SpireHub",
-  description:
-    "SpireHubs offers end-to-end web mobile application development services such as business needs & requirements analysis & custom design architecture. ",
-  alternates: {
-    canonical: "https://www.spirehubs.com",
-  },
-  metadataBase: new URL("https://www.spirehubs.com"),
-  robots: {
-    index: true,
-    follow: true,
-  },
-  openGraph: {
-    title: "Web & Mobile App Development Company in India | SpireHub",
-    description:
-      "SpireHubs offers end-to-end web mobile application development services such as business needs & requirements analysis & custom design architecture. ",
-    url: "https://www.spirehubs.com",
-    siteName: "SpireHub",
-    locale: "en_US",
-    type: "website",
-  },
-};
+
 
 export default function Home() {
   return (
